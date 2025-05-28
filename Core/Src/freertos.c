22b7d37972a1656e1f779ec889c2a0ccb9dcb7b6
danriel1019 <danriel0829@gmail.com>
2025-05-28 21:01:56 +0800

    lcd alerts working

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -6,21 +6,47 @@
 #include "usbd_cdc_if.h"
 #include "dht.h"
 #include "i2c-lcd.h"
+#include "i2c.h"
 #include <stdio.h>
 #include <string.h>
+
+/* Thresholds */
+#define TEMP_ALERT_HIGH     35
+#define HUM_ALERT_HIGH      80
+#define LIGHT_ALERT_LOW     10
+#define LIGHT_ALERT_HIGH    90
 
 /* Task handles */
 osThreadId_t sensorTaskHandle;
 osThreadId_t displayTaskHandle;
+osThreadId_t buttonTaskHandle;
 
-/* Shared sensor data */
-uint8_t temp = 0, hum = 0, lightPercent = 0;
+/* Queue handle */
+osMessageQueueId_t sensorQueueHandle;
+
+/* Display mode */
+volatile uint8_t displayMode = 1;
+
+/* Max recorded values */
+uint8_t maxTemp = 0, maxHum = 0, maxLight = 0;
+
+/* Struct for sensor readings */
+typedef struct {
+  uint8_t temp;
+  uint8_t hum;
+  uint8_t lightPercent;
+  uint16_t rawTemp;
+  uint16_t rawHum;
+  uint16_t adcValue;
+  uint8_t dhtStatus;
+  uint8_t ldrStatus;
+} SensorData_t;
 
 /* Task prototypes */
 void SensorTask(void *argument);
 void DisplayTask(void *argument);
+void ButtonTask(void *argument);
 
-/* FreeRTOS initialization */
 void MX_FREERTOS_Init(void)
 {
     const osThreadAttr_t sensorTask_attributes = {
@@ -36,68 +62,180 @@ void MX_FREERTOS_Init(void)
         .priority = (osPriority_t) osPriorityBelowNormal,
     };
     displayTaskHandle = osThreadNew(DisplayTask, NULL, &displayTask_attributes);
+
+    const osThreadAttr_t buttonTask_attributes = {
+        .name = "ButtonTask",
+        .stack_size = 128 * 4,
+        .priority = (osPriority_t) osPriorityLow,
+    };
+    buttonTaskHandle = osThreadNew(ButtonTask, NULL, &buttonTask_attributes);
+
+    sensorQueueHandle = osMessageQueueNew(4, sizeof(SensorData_t), NULL);
 }
 
-/* Task: Sensor + USB output */
+/* Sensor task */
 void SensorTask(void *argument)
 {
     MX_USB_DEVICE_Init();
     DHT_Init();
-
     char msg[64];
-    uint32_t adcValue = 0;
 
     for (;;)
     {
+        SensorData_t data;
         HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
 
         HAL_ADC_Start(&hadc1);
         if (HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK)
         {
-            adcValue = HAL_ADC_GetValue(&hadc1);
-            lightPercent = (adcValue * 100) / 4095;
+            data.adcValue = HAL_ADC_GetValue(&hadc1);
+            data.lightPercent = (data.adcValue * 100) / 4095;
+            data.ldrStatus = (data.lightPercent > 1 && data.lightPercent < 99);
         }
         HAL_ADC_Stop(&hadc1);
 
-        if (DHT_Read(&hum, &temp) == 0)
+        if (DHT_Read(&data.hum, &data.temp) == 0)
         {
-            snprintf(msg, sizeof(msg), "Light: %u%%, Temp: %dC, Hum: %d%%\r\n", lightPercent, temp, hum);
+            data.dhtStatus = 1;
+            data.rawHum = data.hum * 10;
+            data.rawTemp = data.temp * 10;
+
+            if (data.temp > maxTemp) maxTemp = data.temp;
+            if (data.hum > maxHum) maxHum = data.hum;
+            if (data.lightPercent > maxLight) maxLight = data.lightPercent;
+
+            snprintf(msg, sizeof(msg), "Temp: %dC  Hum: %d%%  Light: %d%%\r\n", data.temp, data.hum, data.lightPercent);
         }
         else
         {
-            snprintf(msg, sizeof(msg), "Light: %u%%, DHT error\r\n", lightPercent);
+            data.dhtStatus = 0;
+            snprintf(msg, sizeof(msg), "DHT sensor error\r\n");
         }
 
         CDC_Transmit_FS((uint8_t *)msg, strlen(msg));
+        osMessageQueuePut(sensorQueueHandle, &data, 0, 0);
         osDelay(1000);
     }
 }
 
-/* Task: LCD display update */
+/* Display task */
 void DisplayTask(void *argument)
 {
-    osDelay(100);  // Ensure power and I2C are stable
+    osDelay(500);
+    HAL_I2C_DeInit(&hi2c1);
+    HAL_I2C_Init(&hi2c1);
+    osDelay(10);
     lcd_init();
+    osDelay(50);
     lcd_clear();
+    osDelay(10);
+    lcd_clear();
+    osDelay(10);
+
+    SensorData_t data;
 
     for (;;)
     {
-        char line1[17];
-        char line2[17];
+        lcd_clear();
 
-        snprintf(line1, sizeof(line1), "T:%dC H:%d%%", temp, hum);
-        snprintf(line2, sizeof(line2), "Light: %3d%%", lightPercent);
+        char line1[17], line2[17], lbuf[14];
+        char lcdAlert1[17] = "";
+        char lcdAlert2[17] = "";
+        uint8_t showAlert = 0;
 
-        lcd_put_cur(0, 0);
-        lcd_send_string("                ");
-        lcd_put_cur(0, 0);
-        lcd_send_string(line1);
+        if (osMessageQueueGet(sensorQueueHandle, &data, NULL, 100) == osOK)
+        {
+            // Priority 1: Sensor errors
+            if (!data.dhtStatus) {
+                snprintf(lcdAlert1, sizeof(lcdAlert1), "DHT SENSOR ERROR");
+                showAlert = 1;
+            }
+            else if (!data.ldrStatus) {
+                snprintf(lcdAlert1, sizeof(lcdAlert1), "LDR SENSOR ERROR");
+                showAlert = 1;
+            }
+            // Priority 2: Environmental thresholds (only if DHT OK)
+            else {
+                if (data.temp > TEMP_ALERT_HIGH) {
+                    snprintf(lcdAlert1, sizeof(lcdAlert1), "TEMP TOO HIGH");
+                    showAlert = 1;
+                }
+                if (data.hum > HUM_ALERT_HIGH) {
+                    if (showAlert)
+                        snprintf(lcdAlert2, sizeof(lcdAlert2), "HUMIDITY TOO HIGH");
+                    else
+                        snprintf(lcdAlert1, sizeof(lcdAlert1), "HUMIDITY TOO HIGH");
+                    showAlert = 1;
+                }
+                if (data.lightPercent < LIGHT_ALERT_LOW) {
+                    if (showAlert && strlen(lcdAlert2) == 0)
+                        snprintf(lcdAlert2, sizeof(lcdAlert2), "LIGHT LEVEL LOW");
+                    else if (!showAlert)
+                        snprintf(lcdAlert1, sizeof(lcdAlert1), "LIGHT LEVEL LOW");
+                    showAlert = 1;
+                }
+                else if (data.lightPercent > LIGHT_ALERT_HIGH) {
+                    if (showAlert && strlen(lcdAlert2) == 0)
+                        snprintf(lcdAlert2, sizeof(lcdAlert2), "LIGHT LEVEL HIGH");
+                    else if (!showAlert)
+                        snprintf(lcdAlert1, sizeof(lcdAlert1), "LIGHT LEVEL HIGH");
+                    showAlert = 1;
+                }
+            }
 
-        lcd_put_cur(1, 0);
-        lcd_send_string("                ");
-        lcd_put_cur(1, 0);
-        lcd_send_string(line2);
+            if (showAlert)
+            {
+                lcd_clear();
+                lcd_put_cur(0, 0); lcd_send_string(lcdAlert1);
+                lcd_put_cur(1, 0); lcd_send_string(lcdAlert2[0] ? lcdAlert2 : "CHECK SYSTEM");
+                osDelay(1000);
+                lcd_clear();
+            }
+
+            /* Normal display */
+            if (displayMode == 1) {
+                snprintf(line1, sizeof(line1), "T:%03u H:%03u", data.rawTemp, data.rawHum);
+                snprintf(lbuf, sizeof(lbuf), "L:%lu", (unsigned long)data.adcValue);
+                snprintf(line2, sizeof(line2), "%-13s[1]", lbuf);
+            }
+            else if (displayMode == 2) {
+                snprintf(line1, sizeof(line1), "T:%02d\xDF""C H:%02d%%", data.temp, data.hum);
+                snprintf(lbuf, sizeof(lbuf), "L:%d%%", data.lightPercent);
+                snprintf(line2, sizeof(line2), "%-13s[2]", lbuf);
+            }
+            else if (displayMode == 3) {
+                snprintf(line1, sizeof(line1), "T:%02d\xDF""C H:%02d%%", maxTemp, maxHum);
+                snprintf(lbuf, sizeof(lbuf), "L:%d%%", maxLight);
+                snprintf(line2, sizeof(line2), "%-13s[3]", lbuf);
+            }
+            else {
+                snprintf(line1, sizeof(line1), "DHT:%s LDR:%s",
+                         data.dhtStatus ? "OK" : "FAIL",
+                         data.ldrStatus ? "OK" : "FAIL");
+                snprintf(line2, sizeof(line2), "System Running");
+            }
+
+            lcd_put_cur(0, 0); lcd_send_string(line1);
+            lcd_put_cur(1, 0); lcd_send_string(line2);
+        }
 
         osDelay(1000);
+    }
+}
+
+/* Button task */
+void ButtonTask(void *argument)
+{
+    uint8_t lastButtonState = 1;
+    for (;;)
+    {
+        uint8_t buttonState = HAL_GPIO_ReadPin(BUTTON_GPIO_Port, BUTTON_Pin);
+        if (lastButtonState == 1 && buttonState == 0)
+        {
+            displayMode++;
+            if (displayMode > 4) displayMode = 1;
+        }
+        lastButtonState = buttonState;
+        osDelay(50);
     }
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,7 +6,6 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
@@ -17,16 +16,42 @@
 #include "usb_device.h"
 #include "gpio.h"
 
+/* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "i2c-lcd.h"
 #include <stdio.h>
 #include <string.h>
 /* USER CODE END Includes */
 
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-void MX_FREERTOS_Init(void); // FreeRTOS task setup
+void MX_FREERTOS_Init(void);
+/* USER CODE BEGIN PFP */
 
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
@@ -37,9 +62,26 @@ void MX_FREERTOS_Init(void); // FreeRTOS task setup
   */
 int main(void)
 {
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
   /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
   SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
@@ -47,22 +89,31 @@ int main(void)
   MX_ADC1_Init();
   MX_TIM10_Init();
   MX_I2C1_Init();
-  MX_USB_DEVICE_Init();
-
   /* USER CODE BEGIN 2 */
   // LCD welcome screen (optional)
 
   /* USER CODE END 2 */
 
-  /* Start FreeRTOS Scheduler */
+  /* Init scheduler */
   osKernelInitialize();
+
+  /* Call init function for freertos objects (in cmsis_os2.c) */
   MX_FREERTOS_Init();
+
+  /* Start scheduler */
   osKernelStart();
 
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    // Control is handled by RTOS tasks
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
   }
+  /* USER CODE END 3 */
 }
 
 /**
@@ -74,9 +125,14 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
+  /** Configure the main internal regulator output voltage
+  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -86,36 +142,58 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 192;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 4;
-
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
     Error_Handler();
+  }
 
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-    Error_Handler();
-}
-
-/**
-  * @brief Error Handler
-  */
-void Error_Handler(void)
-{
-  __disable_irq();
-  while (1)
   {
-    // Blink LED or debug
+    Error_Handler();
   }
 }
 
-#ifdef USE_FULL_ASSERT
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  // Debug print (optional)
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
 }
-#endif
+#endif /* USE_FULL_ASSERT */
